9310595fca92cf219cba572881a30842d85af7ca
Devvicktor <vkosgei001@gmail.com>
2020-06-02 08:38:36 +0300

    my first comit

---
+++ Assets/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+   var typed=$('.typed');
+   $(function(){
+       typed.typed({
+          strings:['Victor kiprotich','Full-stack developer','Designer','Freelancer'],
+          typeSpeed:100,
+          loop:true,
+       })
+   });
+
+});
